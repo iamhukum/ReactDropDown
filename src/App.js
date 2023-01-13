f7e13558eb4672281c4670c0a133c103c140cea9
Vijay Choudhary <scriptyvijay@gmail.com>
2023-01-13 15:35:34 +0530

    completed functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Dropdown from "./dropdown/Dropdown";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			clicked: true,
+			list: ["Yes", "Probably not"],
+		};
+	}
+
+	handleClick = () => {
+		this.setState({ clicked: !this.state.clicked });
+	};
+
+	render() {
+		const { clicked, list } = this.state;
+		return (
+			<>
+				<Dropdown clicked={clicked} list={list} handleClick={this.handleClick} />
+			</>
+		);
+	}
 }
-
-export default App;
